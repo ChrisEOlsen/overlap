c8a0ca8b64d1dde17feaffcc2423343690409adb
Chris Olsen <chrisevjenolsen@gmail.com>
2024-07-29 16:40:43 -0400

    Question dumb pre dev

---
+++ app/logic/set_schedule_algorithm.py
@@ -0,0 +1,11 @@
+def set_schedule():
+    """
+    Questions to ask:
+    1. What format should the data be organized in?
+    2. How much of the contraints/data does the user get to control?
+    3. Where does the data come from?
+    4. What algorithm do you use?
+    5. What will the output look like?
+    6. Do we involve ai?
+    7. How adaptable should this script be? Are contraints maleable?
+    """" 
